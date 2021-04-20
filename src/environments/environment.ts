e3c2aa4b91a69c5653d8afad3ed27cb64c916c9d
warsan <warsan00@mail.ru>
2021-04-20 17:41:02 +0500

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
+// Содержимое файла для текущей среды будет перезаписано во время сборки.
+// Система сборки по умолчанию использует среду разработки, которая использует `environment.ts`, 
+// но если вы сделаете `ng build --env=prod`, то вместо этого будет использоваться `environment.prod.ts`.
+// Список того, какой env сопоставляется с каким файлом, можно найти в `angular-cli.json`.
 
 export const environment = {
   production: false,
